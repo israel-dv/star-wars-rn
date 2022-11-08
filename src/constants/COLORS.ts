ea11e8fa9a0a514b71d84da1150e5fb2e3a7dedd
israel-dv <se.israel.mc@gmail.com>
2022-11-08 13:45:05 -0600

    feat: add personal theme variables
    Signed-off-by: israel-dv <se.israel.mc@gmail.com>

---
+++ src/constants/COLORS.ts
@@ -0,0 +1,7 @@
+export const COLORS = {
+  primaryBackgorund: "rgb(33, 36, 47)",
+  secondaryBackground: "rgb(29, 32, 42)",
+  textPrimaryColor: "rgb(249,201, 98)",
+  textSecondaryColor: "rgb(97, 105, 123)",
+  borderColor: "rgb(213, 255, 124)",
+};
