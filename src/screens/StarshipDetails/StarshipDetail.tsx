ee8421825ec5bab447d9433888c668e552f19257
israel-dv <se.israel.mc@gmail.com>
2022-11-10 11:02:38 -0600

    feat: add starship details

--- src/screens/StarshipDetails/StarshipDetail.tsx
+++ src/screens/StarshipDetails/StarshipDetail.tsx
@@ -3,6 +3,8 @@ import { View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { SafeGoBackLayout } from "../../layouts/SafeGoBackLayout";
+import { starshipStyles } from "./StarshipStyles";
+import { Typography } from "../../components/Typography";
 
 export const StarshipDetails = ({ route }): React.ReactElement => {
   const { starship } = route.params;
@@ -11,7 +13,75 @@ export const StarshipDetails = ({ route }): React.ReactElement => {
 
   return (
     <SafeGoBackLayout onGoBack={goBack} title={"Starship Details"}>
-      <View></View>
+      <View style={starshipStyles.view}>
+        <View style={{ marginVertical: 15 }}>
+          <Typography.Title
+            text={starship.name}
+            fontSize={36}
+            textAlign="center"
+          />
+        </View>
+        <View style={starshipStyles.infoView}>
+          <Typography.TextRegular
+            text={starship.manufacturer}
+            style={{
+              ...starshipStyles.textDetailInfo,
+
+              color: "orange",
+            }}
+          />
+        </View>
+        <View style={starshipStyles.infoView}>
+          <Typography.TextRegular
+            text={"Class: "}
+            style={starshipStyles.textDetail}
+          />
+          <Typography.TextRegular
+            text={starship.starship_class}
+            style={starshipStyles.textDetailInfo}
+          />
+        </View>
+        <View style={starshipStyles.infoView}>
+          <Typography.TextRegular
+            text={"Passengers: "}
+            style={starshipStyles.textDetail}
+          />
+          <Typography.TextRegular
+            text={starship.passengers}
+            style={starshipStyles.textDetailInfo}
+          />
+        </View>
+        <View style={starshipStyles.infoView}>
+          <Typography.TextRegular
+            text={"Cargo Capacity: "}
+            style={starshipStyles.textDetail}
+          />
+          <Typography.TextRegular
+            text={starship.cargo_capacity}
+            style={starshipStyles.textDetailInfo}
+          />
+        </View>
+        <View style={starshipStyles.infoView}>
+          <Typography.TextRegular
+            text={"Crew: "}
+            style={starshipStyles.textDetail}
+          />
+          <Typography.TextRegular
+            text={starship.crew}
+            style={starshipStyles.textDetailInfo}
+          />
+        </View>
+        <View style={starshipStyles.infoView}>
+          <Typography.TextRegular
+            text={"Max Speed: "}
+            style={starshipStyles.textDetail}
+          />
+          <Typography.TextRegular
+            text={starship.max_atmosphering_speed}
+            style={starshipStyles.textDetailInfo}
+          />
+        </View>
+      </View>
     </SafeGoBackLayout>
   );
 };
---
+++ src/screens/StarshipDetails/StarshipStyles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const starshipStyles = StyleSheet.create({
+  view: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+  },
+  infoView: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+  textDetail: {
+    fontSize: 22,
+    color: "rgb(121,195,187)",
+  },
+  textDetailInfo: {
+    fontSize: 22,
+    color: "rgb(213,255,124)",
+  },
+});
